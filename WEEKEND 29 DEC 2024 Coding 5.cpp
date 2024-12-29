6d9743450149f33969f2447d32611f8ecfe9336d
Ajaybalaji2115 <150769063+Ajaybalaji2115@users.noreply.github.com>
2024-12-29 23:16:47 +0530

    Create WEEKEND 29 DEC 2024 Coding 5.cpp

---
+++ WEEKEND 29 DEC 2024 Coding 5.cpp
@@ -0,0 +1,45 @@
+/*Single File Programming Question
+Problem Statement
+
+
+
+The cities in a country are connected as a graph. There should be a path between all pairs of cities. We have to find the maximal strongly connected cities. Given a set of vertices and edges, write a program to identify the strongly connected components in a graph.
+
+
+
+Strongly connected components in a graph are sets of vertices where each vertex in the set can be reached from every other vertex in the same set by following directed edges.
+
+Input format :
+The first line consists of the number of edges n.
+
+The next n lines consist of two integers: a and b representing directed edges from vertex a to vertex b.
+
+Output format :
+The output prints the strongly connected components in the graph, each on a separate line.
+
+The components are printed as space-separated integers representing the vertices in each component.
+
+
+
+Refer to the sample output for the formatting specifications.
+
+Code constraints :
+0 ≤ a, b < n
+
+Sample test cases :
+Input 1 :
+5
+1 0
+0 2
+2 1
+0 3
+3 4
+Output 1 :
+0 1 2 
+3 
+4 
+Note :
+The program will be evaluated only after the “Submit Code” is clicked.
+Extra spaces and new line characters in the program output will result in the failure of the test case.
+Marks : 10
+Negative Marks : 0*/
